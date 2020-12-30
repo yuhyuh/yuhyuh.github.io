d4ab1a6c1b7f5e5f7273d9dedfc10e801cd91cb9
yuhyuh <yuucchaann@gmail.com>
2020-12-30 18:22:21 +0900

    no message

--- js/index.js
+++ js/index.js
@@ -1,13 +1,28 @@
 $(document).ready(function(){
-    var navPos = $('#header-menu').offset().top;
-    var navHeight = $('#header-menu').outerHeight();
+    // メニューバー設定
+    var menuTop = $('#menu').offset().top;
+    var menuHeight = $('#menu').outerHeight();
     $(window).on('scroll', function(){
-      if ($( this).scrollTop() > navPos ) {
-        $('body').css('padding-top', navHeight);
-        $('#header-menu').addClass('m_fixed');
-      } else {
-        $('body').css('padding-top', 0);
-        $('#header-menu').removeClass('m_fixed');
-      }
+        if ($(this).scrollTop() > menuTop) {
+            $('body').css('padding-top', menuHeight);
+            $('#menu').addClass('menu_fixed');
+        } else {
+            $('body').css('padding-top', 0);
+            $('#menu').removeClass('menu_fixed');
+        }
+    });
+
+    // モーダルウィンドウopen
+    $('.modal-open').on('click',function(){
+        var id = $(this).data('target');
+        var modal = document.getElementById(id);
+        $(modal).fadeIn();
+        return false;
+    });
+
+    // モーダルウィンドウclose
+    $('.modal-close').on('click',function(){
+        $('.modal').fadeOut();
+        return false;
     });
 });
